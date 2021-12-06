87f8313611245f1c7a90c348ef8fd946aa521253
nhlong20 <hoanglongmcs1@gmail.com>
2021-12-07 00:26:34 +0700

    devops: add nginx as load balancer

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: `${process.env.API_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_API_ENDPOINT}`
 })
 
 export default instance;
